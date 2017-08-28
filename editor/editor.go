e45cd2bc3a17fd6b8e83873c45652af164165ff8
Thomas Schaffer <loopfz@gmail.com>
2017-08-28 15:27:46 +0200

    Initial

---
+++ editor/editor.go
@@ -0,0 +1,66 @@
+// Package editor lets the user edit either arbitrary text or a JSON representation of an arbitrary object
+// in their text editor of choice (default is vi).
+package editor
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+// Edit runs the user's text editor with arbitrary initial content,
+// then it lets the user edit it and returns the edited result.
+func Edit(content string) (string, error) {
+	filename := fmt.Sprintf("/tmp/tmpeditor%s", uuid.NewV4().String())
+
+	err := ioutil.WriteFile(filename, []byte(content), 0600)
+	if err != nil {
+		return "", err
+	}
+
+	defer os.Remove(filename)
+
+	editorvar := os.Getenv("EDITOR")
+	if editorvar == "" {
+		editorvar = "vi"
+	}
+
+	editorcmd := exec.Command(editorvar, filename)
+	editorcmd.Stdin = os.Stdin
+	editorcmd.Stdout = os.Stdout
+
+	err = editorcmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	edited, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	return string(edited), nil
+}
+
+// EditJSON runs the user's text editor with a JSON representation of input parameter i,
+// then it lets the user edit it and unmarshals the edited result back into i.
+func EditJSON(i interface{}) error {
+	return EditJSONTarget(i, i)
+}
+
+// EditJSONTarget is similar to EditJSON but lets you specify a different target for unmarshaling.
+func EditJSONTarget(i interface{}, target interface{}) error {
+	j, err := json.MarshalIndent(i, "", "    ")
+	if err != nil {
+		return err
+	}
+	edited, err := Edit(string(j))
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal([]byte(edited), target)
+}
---
+++ filemgr/filemgr.go
@@ -0,0 +1,115 @@
+// Package filemgr provides provides file read/write functions using external binaries.
+// This is especially useful if you want to use e.g. gpg, while letting the user override it
+// if necessary.
+package filemgr
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+)
+
+var (
+	// Default to using gpg
+	// use the first private key we find for encryption
+	writeDefault = `gpg --yes --encrypt --recipient $(gpg --list-secret-keys --with-colons | head -1 | cut -d: -f5) -o`
+	readDefault  = `gpg -d -q`
+
+	// Override default r/w cmd through env
+	// e.g.
+	// FILE_READER_CMD="gpg -d -q --passphrase XXX" for scripting + gpg
+	// FILE_WRITER_CMD="cat >" for plain
+	// ...
+	writeCmd = writeDefault
+	readCmd  = readDefault
+
+	lock sync.Mutex
+)
+
+func init() {
+	SetWriteCmd(os.Getenv("FILE_WRITER_CMD"))
+	SetReadCmd(os.Getenv("FILE_READER_CMD"))
+}
+
+// SetWriteCmd overrides the cmd used for writing to files. Lower priority than env FILE_WRITER_CMD (noop if already changed).
+func SetWriteCmd(cmd string) {
+	lock.Lock()
+	if strings.TrimSpace(cmd) != "" && writeCmd == writeDefault {
+		writeCmd = cmd
+	}
+	lock.Unlock()
+}
+
+// SetReadCmd overrides the cmd used for reading files. Lower priority than env FILE_READER_CMD (noop if already changed).
+func SetReadCmd(cmd string) {
+	lock.Lock()
+	if strings.TrimSpace(cmd) != "" && readCmd == readDefault {
+		readCmd = cmd
+	}
+	lock.Unlock()
+}
+
+// ReadFile reads target file using the configured file reader bin
+func ReadFile(path string) (string, error) {
+
+	cmdArgs := []string{"-c", readCmd + " " + path}
+
+	cmd := exec.Command("sh", cmdArgs...)
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return "", err
+	}
+	defer stderr.Close()
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return "", err
+	}
+
+	errout, _ := ioutil.ReadAll(stderr)
+
+	out, _ := ioutil.ReadAll(stdout)
+
+	err = cmd.Wait()
+
+	if err != nil {
+		return "", fmt.Errorf("%s: %s", string(errout), err)
+	}
+
+	return string(out), nil
+}
+
+// WriteFile writes target file (passing content through stdin) using the configured filr writer bin
+func WriteFile(path, content string) error {
+
+	cmdArgs := []string{"-c", writeCmd + " " + path}
+
+	cmd := exec.Command("sh", cmdArgs...)
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, content)
+	}()
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%s: %s", string(out), err)
+	}
+
+	return nil
+}
